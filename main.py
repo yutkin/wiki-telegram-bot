12ae51fb7a20c19b5316249f5fa6df0d4f36ed21
Dmitry Yutkin <yutkinn@gmail.com>
2018-04-09 16:18:18 +0300

    init

---
+++ main.py
@@ -0,0 +1,160 @@
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler
+
+import logging
+
+from emoji import emojize
+from collections import deque
+import shelve
+
+from utils import *
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+GREETING_MSG = 'Привет, {name}! Я {botname}!:waving_hand:\nО чём хочешь узнать?'
+NOT_FOUND_MSG = 'По запросу ничего не найдено! :frowning_face:'
+ERROR_MSG = 'Что-то пошло не так... :thinking_face:'
+MULTIPLE_MEANINGS_MSG = 'Много различных значений, уточните запрос. :winking_face:'
+HISTORY_HAS_BEEN_CLEANED_MSG = 'История запросов очищена.'
+HISTORY_IS_EMPTY_MSG = 'История запросов чиста.'
+HISTORY_LIST_MSG = 'История запросов:'
+
+TOKEN = '494665754:AAGgx5IGizH9ArIy4yq_Vo4xdOkPY74wBF8'
+HISTORY_MAX_LEN = 10
+DB_NAME = 'history.db'
+
+wiki = WikipediaDataSet('articles_meta.csv', 'dataset.npy', 'cc.ru.300.bin')
+logger.info('Wikipedia dataset has been constructed')
+wiki.build_LSH_index()
+logger.info('LSH Index has been built')
+
+
+def start(bot, update):
+    msg = GREETING_MSG.format(name=update.message.from_user.first_name, botname=bot.username)
+    update.message.reply_text(emojize(msg))
+
+
+def history(bot, update):
+    with shelve.open(DB_NAME) as db:
+        hist = db.get(str(update.message.chat_id))
+    if hist:
+        rows = []
+        for i, (title, url) in enumerate(hist):
+            rows.append('{}. {} [{}]'.format(i+1, title, url))
+        update.message.reply_text('{}\n{}'.format(HISTORY_LIST_MSG, '\n'.join(rows)),
+                                  disable_web_page_preview=True)
+    else:
+        update.message.reply_text(HISTORY_IS_EMPTY_MSG)
+
+
+def add_to_history(chat_id, title, url):
+    key = str(chat_id)
+    with shelve.open(DB_NAME) as db:
+        hist = db.get(key)
+
+    hist = hist or deque(maxlen=HISTORY_MAX_LEN)
+    hist.append((title, url))
+
+    with shelve.open(DB_NAME) as db:
+        db[key] = hist
+
+
+def clear_history(bot, update):
+    with shelve.open(DB_NAME) as db:
+        db[str(update.message.chat_id)] = deque(maxlen=HISTORY_MAX_LEN)
+    update.message.reply_text(emojize(HISTORY_HAS_BEEN_CLEANED_MSG))
+
+
+def button_pressed(bot, update):
+    query = update.callback_query
+    query.answer()
+    bot.send_chat_action(chat_id=query.message.chat_id,
+                         action=telegram.ChatAction.TYPING)
+
+    url = WIKI_ARTICLE_URL.format(pageid=query.data)
+    title = get_title_by_url(url)
+    summary = get_wiki_article_summary(query.data)
+
+    summary = summary if summary else ''
+    title = title if title else '?'
+
+    recommended_articles = wiki.find_k_nearest_neighbors(title)
+    keyboard = []
+    for article in recommended_articles:
+        keyboard.append([
+            InlineKeyboardButton(article.title, callback_data=article.pageid)
+        ])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    add_to_history(str(query.message.chat_id), title, url)
+
+    bot.edit_message_text('{}\n{}'.format(url, summary),
+                          chat_id=query.message.chat_id,
+                          message_id=query.message.message_id,
+                          reply_markup=reply_markup,
+                          disable_web_page_preview=True)
+
+
+def query_article(bot, update):
+    bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+    query = update.message.text.strip().lower()
+    try:
+        found = search_wiki_article(query)
+        if found:
+            title, pageid = found
+            url = WIKI_ARTICLE_URL.format(pageid=pageid)
+            summary = get_wiki_article_summary(pageid)
+            if summary:
+                recommended_articles = wiki.find_k_nearest_neighbors(title)
+                keyboard = []
+                for article in recommended_articles:
+                    keyboard.append([
+                        InlineKeyboardButton(article.title, callback_data=article.pageid)
+                    ])
+                reply_markup = InlineKeyboardMarkup(keyboard)
+
+                add_to_history(str(update.message.chat_id), title, url)
+
+                update.message.reply_text('{}\n{}'.format(url, summary),
+                                          reply_markup=reply_markup,
+                                          disable_web_page_preview=True)
+            else:
+                update.message.reply_text(emojize(MULTIPLE_MEANINGS_MSG))
+        else:
+            update.message.reply_text(emojize(NOT_FOUND_MSG))
+    except Exception as AnyException:
+        update.message.reply_text(emojize(ERROR_MSG))
+        logger.error(AnyException)
+
+
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    updater = Updater(TOKEN, request_kwargs={'read_timeout': 30,
+                                             'connect_timeout': 30})
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CallbackQueryHandler(button_pressed))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("hist", history))
+    dp.add_handler(CommandHandler("clear_hist", clear_history))
+    dp.add_handler(MessageHandler(Filters.text, query_article))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling(clean=True)
+
+    logger.info('Bot Started')
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,104 @@
+from bs4 import BeautifulSoup
+import requests
+from collections import namedtuple
+
+import falconn
+import fastText
+import numpy as np
+import pandas as pd
+
+WIKI_ARTICLE_URL = 'https://ru.wikipedia.org/wiki?curid={pageid}'
+WIKI_API_ENDPOINT = 'https://ru.wikipedia.org/w/api.php'
+Article = namedtuple('Article', ['title', 'pageid'])
+
+
+def get_title_by_url(article_url):
+    r = requests.get(article_url)
+    return BeautifulSoup(r.text, 'lxml').find('h1').get_text() if r.ok else None
+
+
+def get_wiki_article_summary(pageid):
+    params = {
+        'action': 'query',
+        'prop': 'extracts|pageprops',
+        'inprop': 'url',
+        'ppprop': 'disambiguation',
+        'exintro': True,
+        'exlimit': 1,
+        'format': 'json',
+        'pageids': pageid
+    }
+    response = requests.get(WIKI_API_ENDPOINT, params=params)
+    response.raise_for_status()
+    response = response.json()
+
+    page = response['query']['pages'][pageid]
+    if 'pageprops' in page:
+        return None
+
+    html = response['query']['pages'][pageid]['extract']
+
+    first_p = None
+    for p in BeautifulSoup(html, 'lxml').find_all('p'):
+        r = p.get_text().strip()
+        if r:
+            first_p = r
+            break
+
+    return first_p.replace('́', '') if first_p else None
+
+
+def search_wiki_article(query):
+    params = {
+        'action': 'query',
+        'srlimit': 1,
+        'list': 'search',
+        'format': 'json',
+        'srinfo': 'totalhits',
+        'srprop': '',
+        'srsearch': query
+    }
+    response = requests.get(WIKI_API_ENDPOINT, params=params)
+    response.raise_for_status()
+    response = response.json()
+    data = response['query']['search']
+    if data:
+        return data[0]['title'], str(data[0]['pageid'])
+    else:
+        return None
+
+
+class WikipediaDataSet:
+    def __init__(self, meta_info, vectorized_articles, fast_text_vectors):
+        self.vectorized_articles = np.load(vectorized_articles)
+        self.meta_info = pd.read_csv(meta_info)
+        self.table = None
+        self.query = None
+        self.fast_text = fastText.load_model(fast_text_vectors)
+
+    def build_LSH_index(self):
+        params_cp = falconn.LSHConstructionParameters()
+        params_cp.dimension = self.vectorized_articles.shape[1]
+        params_cp.lsh_family = falconn.LSHFamily.CrossPolytope
+        params_cp.distance_function = falconn.DistanceFunction.NegativeInnerProduct
+        params_cp.l = 200
+        params_cp.num_rotations = 1
+        params_cp.num_setup_threads = 0
+        params_cp.storage_hash_table = falconn.StorageHashTable.BitPackedFlatHashTable
+
+        falconn.compute_number_of_hash_functions(21, params_cp)
+        self.table = falconn.LSHIndex(params_cp)
+        self.table.setup(self.vectorized_articles)
+
+        self.query = self.table.construct_query_object()
+        self.query.set_num_probes(params_cp.l)
+
+    def find_k_nearest_neighbors(self, title, k=5):
+        q = self.fast_text.get_sentence_vector(title)
+        ids = self.query.find_k_nearest_neighbors(q, k=k)
+        found = self.meta_info.iloc[ids].to_dict(orient='records')
+        result = []
+        for item in found:
+            if item['title'].lower() != title.lower():
+                result.append(Article(title=item['title'], pageid=item['id']))
+        return result[:3]
